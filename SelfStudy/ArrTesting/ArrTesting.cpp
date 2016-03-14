154d77f53a36b05a2eb710f0a0981a5bc1382860
KKM <kakama13@hanmail.net>
2016-03-14 16:42:48 +0900

    Insertion Sort

---
+++ SelfStudy/ArrTesting/ArrTesting.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a);
+
+int main()
+{
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	int i;
+
+	swap(arr);
+
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+
+	return 0;
+}
+
+void swap(int *arr)
+{
+	*(arr+4) = *(arr + 1);
+}
