1f761cda788624ff568515b5b9c58c49f9c52bc3
KKM <kakama13@hanmail.net>
2016-04-07 21:01:39 +0900

    369game commit

---
+++ 160407_Pascal_triangle/160407_Pascal_triangle/369game.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int decide(int x);
+
+int main()
+{
+	int count =0;
+	int i;
+	int start, end;
+
+	do {
+		printf("Set START, and END : ");
+		scanf_s("%d %d", &start, &end);
+	} while (start < 1 || start >= end);
+	
+	for (i = start; i <= end; i++)
+	{
+		if (i % 3 == 0) count++;
+		else count += decide(i);
+
+		printf("%d ", i);
+		printf("Clap count : %d\n", count);
+
+	}
+
+	printf("Clap count : %d\n", count);
+
+	return 0;
+}
+
+
+int decide(int x)
+{
+	int sw = 0;
+	int temp;
+
+	while (1)
+	{
+		temp = x % 10;
+
+		if (temp % 3 == 0 && temp != 0) return 1;
+		else if (x >= 10) x = x / 10;
+		else return 0;
+	}
+
+}
